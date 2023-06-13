d289b35e39d3ac97395fe0d94bdeea918b0c06a2
Dragan Miljanovic <dragan.miljanovic@q.agency>
2023-06-13 21:03:01 +0200

    sh script for resetting the database: unneeded migration removed

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -13,7 +13,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -25,7 +25,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => 'required|email',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:4',
         ];
     }
 }
